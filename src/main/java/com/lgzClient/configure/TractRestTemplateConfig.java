e4d703148bbe4a48baea42a087ae482c4af3e750
罗广展 <2192947642@qq.com>
2025-02-09 22:02:33 +0800

    对所依赖的restTemplate进行注入

--- src/main/java/com/lgzClient/configure/TractRestTemplateConfig.java
+++ src/main/java/com/lgzClient/configure/TractRestTemplateConfig.java
@@ -2,8 +2,9 @@ package com.lgzClient.configure;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-
+@Configuration
 public class TractRestTemplateConfig {
     @Bean("tractRestTemplate")
     @LoadBalanced

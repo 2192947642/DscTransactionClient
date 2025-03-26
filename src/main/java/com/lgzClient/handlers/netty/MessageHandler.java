bae9b5a6d1a23abf18f2a6c901e21cdad34b39f9
罗广展 <14077231+luo-guangzhan@user.noreply.gitee.com>
2025-03-26 08:59:42 +0800

    if (transactContainer == null) return;//如果没有事务容器,那么就直接返回

--- src/main/java/com/lgzClient/handlers/netty/MessageHandler.java
+++ src/main/java/com/lgzClient/handlers/netty/MessageHandler.java
@@ -16,21 +16,22 @@ public class MessageHandler extends SimpleChannelInboundHandler<String> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, String string) throws Exception {
-         Message msg= JsonUtil.jsonToObject(string,Message.class);
-        if(msg.getType() == MessageTypeEnum.GlobalNotice){
+        Message msg = JsonUtil.jsonToObject(string, Message.class);
+        if (msg.getType() == MessageTypeEnum.GlobalNotice) {
             handleGlobalNotice(msg);
         }
     }
+
     //收到全局的事务状态通知
     public void handleGlobalNotice(Message message) throws SQLException {
-        GlobalNotice globalNotice= JsonUtil.jsonToObject(message.getContent(),GlobalNotice.class);
-        LocalTransactionManager instance= LocalTransactionManager.instance;
-        TransactContainer transactContainer=instance.getTransactionContainerById(globalNotice.getBranchId());
-        if(transactContainer==null){//如果没有事务容器,那么就直接返回
-            if(globalNotice.getIsSuccess())//如果全局事务成功 那么就提交事务
-              instance.commitByThreadPoolAndWebFlux(transactContainer.getBranchTransaction());
-            else instance.rollBackByThreadPoolAndWebFlux(transactContainer.getBranchTransaction());//回滚事务
-        }
+        GlobalNotice globalNotice = JsonUtil.jsonToObject(message.getContent(), GlobalNotice.class);
+        LocalTransactionManager instance = LocalTransactionManager.instance;
+        TransactContainer transactContainer = instance.getTransactionContainerById(globalNotice.getBranchId());
+        if (transactContainer == null) return;//如果没有事务容器,那么就直接返回
+        if (globalNotice.getIsSuccess())//如果全局事务成功 那么就提交事务
+            instance.commitByThreadPoolAndWebFlux(transactContainer.getBranchTransaction());
+        else instance.rollBackByThreadPoolAndWebFlux(transactContainer.getBranchTransaction());//回滚事务
+
 
     }
 }
